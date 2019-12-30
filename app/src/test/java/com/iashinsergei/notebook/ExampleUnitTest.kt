02a2f8d5fdd1188de609b804884ce2283e63c789
mr.Blue <yashinsergey125@gmail.com>
2019-12-30 15:35:16 +0300

    rename package

--- app/src/test/java/com/iashinsergei/notebook/ExampleUnitTest.kt
+++ app/src/test/java/com/iashinsergei/notebook/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.example.notebook
+package com.iashinsergei.notebook
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
