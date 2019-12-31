6ca77f173426edce46a286f67c40d446780240ac
mr.Blue <yashinsergey125@gmail.com>
2019-12-31 18:08:08 +0300

    replaced String to MainViewState

--- app/src/main/java/com/iashinsergei/notebook/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/iashinsergei/notebook/ui/viewmodels/MainViewModel.kt
@@ -3,15 +3,17 @@ package com.iashinsergei.notebook.ui.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.iashinsergei.notebook.data.NotesRepository
+import com.iashinsergei.notebook.ui.viewstates.MainViewState
 
 class MainViewModel : ViewModel() {
 
-    private val viewStateLiveData : MutableLiveData<String> = MutableLiveData()
+    private val viewStateLiveData : MutableLiveData<MainViewState> = MutableLiveData()
 
     init {
-        viewStateLiveData.value = "Testing"
+        viewStateLiveData.value = MainViewState(NotesRepository.getNotes())
     }
 
-    fun viewState(): LiveData<String> = viewStateLiveData
+    fun viewState(): LiveData<MainViewState> = viewStateLiveData
 
 }
