7867e59636c47e261b4324a97af77a208d76238e
mr.Blue <yashinsergey125@gmail.com>
2019-12-31 18:09:10 +0300

    Added initTollbar and initRecyclerView methods

--- app/src/main/java/com/iashinsergei/notebook/ui/activities/MainActivity.kt
+++ app/src/main/java/com/iashinsergei/notebook/ui/activities/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.iashinsergei.notebook.ui.activities
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.iashinsergei.notebook.R
+import com.iashinsergei.notebook.ui.adapters.RvAdapter
 import com.iashinsergei.notebook.ui.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter: RvAdapter
     lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+
+        initRecyclerView()
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.viewState().observe(this, Observer {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            it.let { adapter.notes = it.notes}
         })
+    }
+
+    private fun initRecyclerView() {
+        rec_view_notes.layoutManager = GridLayoutManager(this, 2)
+        adapter = RvAdapter()
+        rec_view_notes.adapter = adapter
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        collapsingToolbarLayout.isTitleEnabled = false
+        supportActionBar!!.title = "Notebook"
     }
 }
