8ad763a1d35be908f427bbfe6668857b0c651835
mr.Blue <yashinsergey125@gmail.com>
2019-12-31 16:25:04 +0300

    Added note repository

---
+++ app/src/main/java/com/iashinsergei/notebook/data/entity/NotesRepository.kt
@@ -0,0 +1,15 @@
+package com.iashinsergei.notebook.data.entity
+
+import android.graphics.Color
+
+object NotesRepository {
+    private val notes: List<Note> = listOf(
+        Note("First note", "This is first note", Color.BLUE),
+        Note("Second note", "This is second note", Color.RED),
+        Note("Third note", "This is third note", Color.CYAN),
+        Note("Fourth note", "This is fourth note", Color.GRAY),
+        Note("Fifth note", "This is fifth note", Color.GREEN),
+        Note("Sixth note", "This is sixth note", Color.MAGENTA)
+    )
+
+}
