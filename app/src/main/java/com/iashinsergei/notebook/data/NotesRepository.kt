84871702b7558714ed2a185e81c98e06a10c2330
mr.Blue <yashinsergey125@gmail.com>
2019-12-31 18:36:17 +0300

    changed colors

--- app/src/main/java/com/iashinsergei/notebook/data/NotesRepository.kt
+++ app/src/main/java/com/iashinsergei/notebook/data/NotesRepository.kt
@@ -1,6 +1,5 @@
 package com.iashinsergei.notebook.data
 
-import android.graphics.Color
 import com.iashinsergei.notebook.data.entity.Note
 
 object NotesRepository {
@@ -8,33 +7,33 @@ object NotesRepository {
     private val notes = listOf(
         Note(
             "First note",
-            "This is first note",
-            Color.BLUE
+            "This is first note field. Here you can record your thoughts and ideas.",
+            0xfff06292.toInt()
         ),
         Note(
             "Second note",
-            "This is second note",
-            Color.RED
+            "This is second note field. Here you can record your thoughts and ideas.",
+            0xff9575cd.toInt()
         ),
         Note(
             "Third note",
-            "This is third note",
-            Color.CYAN
+            "This is third note field. Here you can record your thoughts and ideas.",
+            0xff64b5f6.toInt()
         ),
         Note(
             "Fourth note",
-            "This is fourth note",
-            Color.GRAY
+            "This is fourth note field. Here you can record your thoughts and ideas.",
+            0xff4db6ac.toInt()
         ),
         Note(
             "Fifth note",
-            "This is fifth note",
-            Color.GREEN
+            "This is fifth note field. Here you can record your thoughts and ideas.",
+            0xffb2ff59.toInt()
         ),
         Note(
             "Sixth note",
-            "This is sixth note",
-            Color.MAGENTA
+            "This is sixth note field. Here you can record your thoughts and ideas.",
+            0xffffeb3b.toInt()
         )
     )
 
