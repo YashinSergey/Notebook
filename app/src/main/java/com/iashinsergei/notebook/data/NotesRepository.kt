0b2d9fac67b1a32ef2c6492c88c57fcbe1b7ab25
mr.Blue <yashinsergey125@gmail.com>
2020-01-01 18:09:13 +0300

    added parameters to class Note

--- app/src/main/java/com/iashinsergei/notebook/data/NotesRepository.kt
+++ app/src/main/java/com/iashinsergei/notebook/data/NotesRepository.kt
@@ -1,33 +1,46 @@
 package com.iashinsergei.notebook.data
 
 import com.iashinsergei.notebook.data.entity.Note
+import java.util.*
 
 object NotesRepository {
 
     private val notes = listOf(
         Note(
+            UUID.randomUUID().toString(),
             "First note",
-            "This is first note field. Here you can record your thoughts and ideas."
+            "This is first note field. Here you can record your thoughts and ideas.",
+            null
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Second note",
-            "This is second note field. Here you can record your thoughts and ideas."
+            "This is second note field. Here you can record your thoughts and ideas.",
+            null
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Third note",
-            "This is third note field. Here you can record your thoughts and ideas."
+            "This is third note field. Here you can record your thoughts and ideas.",
+            null
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Fourth note",
-            "This is fourth note field. Here you can record your thoughts and ideas."
+            "This is fourth note field. Here you can record your thoughts and ideas.",
+            null
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Fifth note",
-            "This is fifth note field. Here you can record your thoughts and ideas."
+            "This is fifth note field. Here you can record your thoughts and ideas.",
+            null
         ),
         Note(
+            UUID.randomUUID().toString(),
             "Sixth note",
-            "This is sixth note field. Here you can record your thoughts and ideas."
+            "This is sixth note field. Here you can record your thoughts and ideas.",
+            null
         )
     )
 
--- app/src/main/java/com/iashinsergei/notebook/data/entity/Note.kt
+++ app/src/main/java/com/iashinsergei/notebook/data/entity/Note.kt
@@ -1,3 +1,35 @@
 package com.iashinsergei.notebook.data.entity
 
-class Note(val title: String, val text: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Note(val id: String, val title: String, val text: String, val color: Int?) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if(this === other) return true
+        if(javaClass != other?.javaClass) return false
+
+        other as Note
+        if(id != other.id) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + text.hashCode()
+        result = 31 * result + (color ?: 0)
+        return result
+    }
+
+    enum class Color {
+        DEFAULT_THEME_COLOR,
+        ORANGE_THEME_COLOR,
+        BLUE_THEME_COLOR,
+        BROWN_THEME_COLOR,
+        YELLOW_THEME_COLOR,
+        IRIS_BLUE_THEME_COLOR,
+        DARK_THEME_COLOR
+    }
+}
