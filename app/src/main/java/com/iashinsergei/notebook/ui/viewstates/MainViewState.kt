fc8482bcc8189864b0fd4908f6ffcf476ec77697
mr.Blue <yashinsergey125@gmail.com>
2020-01-01 18:45:43 +0300

    replace class with data class

--- app/src/main/java/com/iashinsergei/notebook/ui/viewstates/MainViewState.kt
+++ app/src/main/java/com/iashinsergei/notebook/ui/viewstates/MainViewState.kt
@@ -2,4 +2,4 @@ package com.iashinsergei.notebook.ui.viewstates
 
 import com.iashinsergei.notebook.data.entity.Note
 
-class MainViewState(val notes: List<Note>)
+data class MainViewState(val notes: List<Note>)
