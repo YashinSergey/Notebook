9b50acc98d75acd00e4198d36a9298beb18cd5bf
mr.Blue <yashinsergey125@gmail.com>
2020-01-02 17:26:57 +0300

    added BaseActivity

---
+++ app/src/main/java/com/iashinsergei/notebook/ui/viewmodels/BaseViewModel.kt
@@ -0,0 +1,11 @@
+package com.iashinsergei.notebook.ui.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.iashinsergei.notebook.ui.viewstates.BaseViewState
+
+open class BaseViewModel <T, S: BaseViewState<T>> : ViewModel() {
+    open val viewStateLiveData = MutableLiveData<S>()
+    open fun getViewState(): LiveData<S> = viewStateLiveData
+}
---
+++ app/src/main/java/com/iashinsergei/notebook/ui/viewstates/BaseViewState.kt
@@ -0,0 +1,3 @@
+package com.iashinsergei.notebook.ui.viewstates
+
+open class BaseViewState<T>(val data: T, val error: Throwable?)
