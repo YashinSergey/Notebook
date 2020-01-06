ea20843adbb5727e455b3f1c3e54e1a0a46c268c
mr.Blue <yashinsergey125@gmail.com>
2020-01-06 18:18:18 +0300

    modified NoteViewState with isDeleted flag

--- app/src/main/java/com/sergeiiashin/notebook/ui/viewstates/NoteViewState.kt
+++ app/src/main/java/com/sergeiiashin/notebook/ui/viewstates/NoteViewState.kt
@@ -2,4 +2,6 @@ package com.sergeiiashin.notebook.ui.viewstates
 
 import com.sergeiiashin.notebook.data.entity.Note
 
-class NoteViewState(note: Note? = null, error: Throwable? = null) : BaseViewState<Note?>(note, error)
+class NoteViewState(data: Data = Data(), error: Throwable? = null) : BaseViewState<NoteViewState.Data>(data, error) {
+    data class Data(val isDeleted: Boolean = false, val note: Note? = null)
+}
